c7244364be3bd281765189e53cb090f70f96e59d
mannyk <man962@yahoo.com>
2023-03-02 15:55:35 -0800

    python 2.7 to 3 print.
    1. KMeans API update to remove warnings.
    2. time.clock -> time.time.

--- simul_example.py
+++ simul_example.py
@@ -26,44 +26,45 @@ n_iter = 10 # number of iterations to run
 var = True # wheather to use Gaussian with non-constant variance
 
 ## Generating NC model. Z is the data, rest of the output is used to evaluate the fit
-print '\nRunning NC simulations'
+print('\nRunning NC simulations') 
 h_atoms, h_probs, labels_M, atoms_M, g_probs, Z = simulate_mixture_nc(K, K_a, d, N, M, k, var=var)
 truth = [h_atoms, h_probs, atoms_M, g_probs]
 
 # Fitting NC
-print '\nFitting MWM'
+print('\nFitting MWM') 
 nc_cluster = W_means(K=K, K_a=K_a, k=k, n_iter=n_iter, method='NC', verbose=1).fit(Z)
-print 'NC Wasserstain distance to true model is %f' % -nc_cluster.score(truth)
+print('NC Wasserstain distance to true model is %f' % -nc_cluster.score(truth)) 
 nc_loc_labels = nc_cluster.loc_label(Z)
 
 # Fitting LC
-print '\nFitting MWMS'
+print('\nFitting MWMS')
 lc_cluster = W_means(K=K, K_a=K_a, k=k_S, n_iter=n_iter, method='LC', verbose=1).fit(Z)
-print 'LC Wasserstain distance to true model is %f' % -lc_cluster.score(truth)
+print('LC Wasserstain distance to true model is %f' % -lc_cluster.score(truth)) 
 lc_loc_labels = lc_cluster.loc_label(Z)
 
 # Fitting multistage k-means
-print '\nFitting 3-stage K-means'
+print('\nFitting 3-stage K-means') 
 k_cluster = W_means(K=K, K_a=K_a, k=k, n_iter=n_iter, method='3means', verbose=1).fit(Z)
-print '3-means Wasserstain distance to true model is %f' % -k_cluster.score(truth)
+print('3-means Wasserstain distance to true model is %f' % -k_cluster.score(truth)) 
 k_loc_labels = k_cluster.loc_label(Z)
 
 ## Generating LC model. Z is the data, rest of the output is used to evaluate the fit
-print '\nRunning LC simulations'
+print('\nRunning LC simulations') 
 h_atoms, h_probs, labels_M, S_set, g_probs, Z = simulate_mixture_lc(K, K_a, d, N, M, k_S, var=var)
 truth = [h_atoms, h_probs, M*[S_set], g_probs]
 
 # Fitting NC
-print '\nFitting MWM'
+print('\nFitting MWM') 
 nc_cluster = W_means(K=K, K_a=K_a, k=k, n_iter=n_iter, method='NC', verbose=1).fit(Z)
-print 'NC Wasserstain distance to true model is %f' % -nc_cluster.score(truth)
+print('NC Wasserstain distance to true model is %f' % -nc_cluster.score(truth)) 
 
 # Fitting LC
-print '\nFitting MWMS'
+print('\nFitting MWMS') 
 lc_cluster = W_means(K=K, K_a=K_a, k=k_S, k_init=k, n_iter=n_iter, method='LC', verbose=1).fit(Z)
-print 'LC Wasserstain distance to true model is %f' % -lc_cluster.score(truth)
+print('LC Wasserstain distance to true model is %f' % -lc_cluster.score(truth)) 
 
 # Fitting multistage k-means
-print '\nFitting 3-stage K-means'
+print('\nFitting 3-stage K-means') 
 k_cluster = W_means(K=K, K_a=K_a, k=k, n_iter=n_iter, method='3means', verbose=1).fit(Z)
-print '3-means Wasserstain distance to true model is %f' % -k_cluster.score(truth)
+print('3-means Wasserstain distance to true model is %f' % -k_cluster.score(truth))
+ 
